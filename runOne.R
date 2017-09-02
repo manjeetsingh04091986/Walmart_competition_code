415741190872cd45f56890a61b7a3d53feea75fc
Manjeet Singh <man041986@gmail.com>
2017-09-02 12:55:03 +0530

    one.R
    Same file

--- runOne.R
+++ runOne.R
@@ -5,6 +5,7 @@ source('postprocess.R')
 train <- raw.train()
 test <- raw.test()
 
+
 # This is the best single model from the competition.
 pred <- grouped.forecast(train, test, 'stlf.svd', model.type='ets', n.comp=12)
 pred <- postprocess(train, pred, shift=2.5)
